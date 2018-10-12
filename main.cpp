55a75b9c6d2bc8c774210ee97209969497c1ff95
Utilisateur <user@debian.arcahe.ovh>
2018-10-12 16:03:23 +0200

    petit changement de param dans le main

--- main.cpp
+++ main.cpp
@@ -9,15 +9,19 @@ using namespace std;
 int main(int argc, char** argv)
 {
 	mpf_t x, y, w, h;
-	mpf_init_set_d( x, -0.5);
-	mpf_init_set_d( y, 0.0);
-	mpf_init_set_d( w, 3);
-	mpf_init_set_d( h, 2.0);
+	// mpf_init_set_d( x, -0.5);
+	// mpf_init_set_d( y, 0.0);
+	// mpf_init_set_d( w, 3);
+	// mpf_init_set_d( h, 2.0);
 
-	//Mandelbrot M(x,y,w,h,1920,1080,1);
-	Mandelbrot M(x,y,w,h,480,270,1);
-	
-	M.draw(50);
+	mpf_init_set_d( x, -1.5);
+	mpf_init_set_d( y, 0.0);
+	mpf_init_set_d( w, 0.5);
+	mpf_init_set_d( h, 0.5);
+
+	Mandelbrot M(x,y,w,h,1920,1080,1);
+
+	M.draw(200);
 	
 	mpf_clears( x, y, w, h, NULL);
 	exit(0);

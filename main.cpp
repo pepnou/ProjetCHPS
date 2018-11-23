85e983b7df4110d8767348764fde7e9126c78a52
pepnou <pepnou1@gmail.com>
2018-11-23 13:32:56 +0100

    option en cours

--- main.cpp
+++ main.cpp
@@ -20,7 +20,7 @@ int main(int argc, char** argv)
 		po::options_description generic("Generic options");
 		generic.add_options()
 			("help,h", ": describe arguments")
-			("verbose,v", "be verbose")
+			("verbose,v", ": be verbose")
 			;
 
 		po::options_description config("Configuration");
@@ -53,73 +53,145 @@ int main(int argc, char** argv)
 
 		po::variables_map vm;
 
-        ifstream ifs("Config.cfg");
-        store(parse_config_file(ifs, config_file_options), vm);
-        notify(vm);
+		ifstream ifs("Config.cfg");
+		store(parse_config_file(ifs, config_file_options), vm);
+		notify(vm);
 
-        if (vm.count("Xposition"))
-        {
+		if (vm.count("Xposition"))
+		{
 			mpf_set_str( x, vm["Xposition"].as<string>().c_str(), 10);
 		}
 
 		if (vm.count("Yposition"))
-        {
+		{
 			mpf_set_str( y, vm["Yposition"].as<string>().c_str(), 10);
 		}
 		
 		if (vm.count("im-width"))
-        {
+		{
 			im_w = vm["im-width"].as<int>();
 		}
 		
 		if (vm.count("im-height"))
-        {
+		{
 			im_h = vm["im-height"].as<int>();
 		}
 
 		if (vm.count("width"))
-        {
+		{
 			mpf_set_str( w, vm["width"].as<string>().c_str(), 10);
 		}
 		
 		if (vm.count("height"))
-        {
+		{
 			mpf_set_str( h, vm["height"].as<string>().c_str(), 10);
 		}
 		
 		if (vm.count("color"))
-        {
+		{
 			color = vm["color"].as<int>();
 		}
 		
 		if (vm.count("enough"))
-        {
+		{
 			enough = vm["enough"].as<int>();
 		}
 		
 		if (vm.count("super-sampling"))
-        {
+		{
 			surech = vm["super-sampling"].as<int>();
 		}
 		
-		/*po::positional_options_description positional;
+		po::positional_options_description positional;
 
-		po::variables_map vm;
+		po::variables_map vm2;
 		po::store(po::command_line_parser(argc, argv)
-			.options(options)
+			.options(cmdline_options)
 			.positional(positional)
 			.run(),
-			vm);
-		po::notify(vm);
+			vm2);
+		po::notify(vm2);
 
-		if (vm.count("help")) {
-		std::cout << options << "\n";
-		exit(0);
-		}*/
+		if (vm2.count("help"))
+		{
+			std::cout << visible << "\n";
+			exit(0);
+		}
+
+		if (vm2.count("Xposition"))
+		{
+			mpf_set_str( x, vm2["Xposition"].as<string>().c_str(), 10);
+		}
+
+		if (vm2.count("Yposition"))
+		{
+			mpf_set_str( y, vm2["Yposition"].as<string>().c_str(), 10);
+		}
+		
+		if (vm2.count("im-width"))
+		{
+			im_w = vm2["im-width"].as<int>();
+		}
+		
+		if (vm2.count("im-height"))
+		{
+			im_h = vm2["im-height"].as<int>();
+		}
+
+		if (vm2.count("width"))
+		{
+			mpf_set_str( w, vm2["width"].as<string>().c_str(), 10);
+		}
+		
+		if (vm2.count("height"))
+		{
+			mpf_set_str( h, vm2["height"].as<string>().c_str(), 10);
+		}
+		
+		if (vm2.count("color"))
+		{
+			color = vm2["color"].as<int>();
+		}
+		
+		if (vm2.count("enough"))
+		{
+			enough = vm2["enough"].as<int>();
+		}
+		
+		if (vm2.count("super-sampling"))
+		{
+			surech = vm2["super-sampling"].as<int>();
+		}
+
+
+		char char_width[1002];
+		char char_height[1002];
+		char char_x[1002];
+		char char_y[1002];
+
+		dbg
+
+		mpf_get_str( char_width, NULL, 10, 1000, w);
+		mpf_get_str( char_height, NULL, 10, 1000, h);
+		mpf_get_str( char_x, NULL, 10, 1000, x);
+		mpf_get_str( char_y, NULL, 10, 1000, y);
+
+		dbg
+
+		ofstream ofs("Config.cfg",ofstream::trunc);
+		ofs << "Xposition=" << char_x << "\n"
+			<< "Yposition=" << char_y << "\n"
+			<< "im-width=" << im_w << "\n"
+			<< "im-height=" << im_h << "\n"
+			<< "width=" << char_width << "\n"
+			<< "height=" << char_height << "\n"
+			<< "color=" << color << "\n"
+			<< "enough=" << enough << "\n"
+			<< "super-sampling=" << surech << "\n";
 	}
 	catch(std::exception& E)
 	{
-	    std::cout << E.what() << std::endl;
+		std::cout << E.what() << std::endl;
 	}
 	
 	/*mpf_t x, y, w, h;

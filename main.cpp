5a351f519127425b9d78bb826f5e0ca116de48d2
pepnou <pepnou1@gmail.com>
2018-11-23 18:26:12 +0100

    option en ligne de commande

--- main.cpp
+++ main.cpp
@@ -10,10 +10,15 @@ int main(int argc, char** argv)
 	int im_w = 1920, im_h = 1080, surech = 4, iteration = 100, enough = 1, color = RAINBOW;
 	
 	mpf_t x, y, w, h;
-	mpf_init_set_d( x, -0.5);
+	/*mpf_init_set_d( x, -0.5);
 	mpf_init_set_d( y, 0.0);
 	mpf_init_set_d( w, 3);
-	mpf_init_set_d( h, 2);
+	mpf_init_set_d( h, 2);*/
+
+	mpf_init_set_str( x, "-0.5", 10);
+	mpf_init_set_str( y, "0e30", 10);
+	mpf_init_set_str( w, "3", 10);
+	mpf_init_set_str( h, "2", 10);
 
 	try
 	{
@@ -59,7 +64,10 @@ int main(int argc, char** argv)
 
 		if (vm.count("Xposition"))
 		{
+			cout<<vm["Xposition"].as<string>().c_str()<<endl;
 			mpf_set_str( x, vm["Xposition"].as<string>().c_str(), 10);
+			mpf_out_str(NULL, 10, 100000, x);
+
 		}
 
 		if (vm.count("Yposition"))
@@ -101,15 +109,19 @@ int main(int argc, char** argv)
 		{
 			surech = vm["super-sampling"].as<int>();
 		}
+
+
 		
 		po::positional_options_description positional;
 
 		po::variables_map vm2;
+
 		po::store(po::command_line_parser(argc, argv)
 			.options(cmdline_options)
 			.positional(positional)
 			.run(),
 			vm2);
+
 		po::notify(vm2);
 
 		if (vm2.count("help"))
@@ -163,28 +175,35 @@ int main(int argc, char** argv)
 			surech = vm2["super-sampling"].as<int>();
 		}
 
+		mp_exp_t e1, e2, e3, e4;
+		char *char_width, *char_height, *char_x, *char_y;
+		char tmpx[3] = {'0','.','\0'}, tmpy[3] = {'0','.','\0'};
 
-		char char_width[1002];
-		char char_height[1002];
-		char char_x[1002];
-		char char_y[1002];
 
-		dbg
-
-		mpf_get_str( char_width, NULL, 10, 1000, w);
-		mpf_get_str( char_height, NULL, 10, 1000, h);
-		mpf_get_str( char_x, NULL, 10, 1000, x);
-		mpf_get_str( char_y, NULL, 10, 1000, y);
-
-		dbg
+		char_x = mpf_get_str( NULL, &e1, 10, 1000, x);
+		if(char_x[0] == '-')
+		{
+			char_x[0] = '.';
+			tmpx[0] = '-';
+			tmpx[1] = '0';
+		}
+		char_y = mpf_get_str( NULL, &e2, 10, 1000, y);
+		if(char_y[0] == '-')
+		{
+			char_y[0] = '.';
+			tmpy[0] = '-';
+			tmpy[1] = '0';
+		}
+		char_width = mpf_get_str( NULL, &e3, 10, 1000, w);
+		char_height = mpf_get_str( NULL, &e4, 10, 1000, h);
 
 		ofstream ofs("Config.cfg",ofstream::trunc);
-		ofs << "Xposition=" << char_x << "\n"
-			<< "Yposition=" << char_y << "\n"
+		ofs << "Xposition=" << tmpx << char_x << "e" << e1 << "\n"
+			<< "Yposition=" << tmpy << char_y << "e" << e2 << "\n"
 			<< "im-width=" << im_w << "\n"
 			<< "im-height=" << im_h << "\n"
-			<< "width=" << char_width << "\n"
-			<< "height=" << char_height << "\n"
+			<< "width=" << "0." << char_width << "e" << e3 << "\n"
+			<< "height=" << "0." << char_height << "e" << e4 << "\n"
 			<< "color=" << color << "\n"
 			<< "enough=" << enough << "\n"
 			<< "super-sampling=" << surech << "\n";
@@ -193,6 +212,8 @@ int main(int argc, char** argv)
 	{
 		std::cout << E.what() << std::endl;
 	}
+
+
 	
 	/*mpf_t x, y, w, h;
 	const int NBR_THREADS = std::thread::hardware_concurrency();

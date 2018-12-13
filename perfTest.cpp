1a7064f72f244b1efc72e67ba623ac0e6d63acf5
pepnou <pepnou1@gmail.com>
2018-12-13 09:24:07 +0100

    modif test gmp

--- perfTest.cpp
+++ perfTest.cpp
@@ -19,12 +19,15 @@ using namespace cv;
 
 int main(int argc, char** argv)
 {
-	/*int w=1000, h=1000, nbr_test=10000;
+	int nbr_test=10000;
+	uint64_t tick, total;
+	
+	/*
+	int w=1000, h=1000;
 	Mat* mat;
 	int** tab;
 	int* tab2;
-	uint64_t tick;
-	
+
 	tick = rdtsc();
 	for(int i; i< nbr_test; i++)
 	{
@@ -60,7 +63,47 @@ int main(int argc, char** argv)
 	cout<<"Cout allocation/deallocation int*"<<(rdtsc() - tick)/nbr_test<<endl;*/
 	
 	
+	/*gmp_randstate_t state;
+	gmp_randinit_default (state);
+	int prec = 2048;
+
+	mpf_t a,b,c;
+	mpf_init2( a, 2048);
+	mpf_init2( b, 2048);
+	mpf_init2( c, 2048);
+
+	total = 0;
+	for(int i=0; i<nbr_test;i++)
+	{
+		mpf_urandomb( a, state, prec);
+		mpf_urandomb( b, state, prec);
+		
+		tick = rdtsc();
+
+		total += rdtsc() - tick(); 
+	}
+	cout << ""
+
 	
-	
+
+	mpf_add(mpf t rop, const mpf t op1, const mpf t op2) 
+	mpf_add_ui (mpf t rop, const mpf t op1, unsigned long int op2) 
+	mpf_sub (mpf t rop, const mpf t op1, const mpf t op2) 
+	mpf_ui_sub (mpf t rop, unsigned long int op1, const mpf t op2) 
+	mpf_sub_ui (mpf t rop, const mpf t op1, unsigned long int op2) 
+	mpf_mul (mpf t rop, const mpf t op1, const mpf t op2) 
+	mpf_mul_ui (mpf t rop, const mpf t op1, unsigned long int op2) 
+	mpf_div (mpf t rop, const mpf t op1, const mpf t op2) 
+	mpf_ui_div (mpf t rop, unsigned long int op1, const mpf t op2) 
+	mpf_div_ui (mpf t rop, const mpf t op1, unsigned long int op2) 
+	mpf_sqrt (mpf t rop, const mpf t op) 
+	mpf_sqrt_ui (mpf t rop, unsigned long int op) 
+	mpf_pow_ui (mpf t rop, const mpf t op1, unsigned long int op2) 
+	mpf_neg (mpf t rop, const mpf t op) 
+	mpf_abs (mpf t rop, const mpf t op) 
+	mpf_mul_2exp (mpf t rop, const mpf t op1, mp bitcnt t op2) 
+	mpf_div_2exp (mpf t rop, const mpf t op1, mp bitcnt t op2)*/
+
+
 	exit(0);
 }

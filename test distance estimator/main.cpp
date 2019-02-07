361b37f6c70719f47101f36af1fa302bb9cd12d1
pepnou <pepnou1@gmail.com>
2019-02-07 22:45:03 +0100

    rien de particulier, juste pour push

--- test distance estimator/main.cpp
+++ test distance estimator/main.cpp
@@ -4,7 +4,7 @@
 
 int main()
 {
-	int im_width = 5, im_height = 5, iteration = 10;
+	int im_width = 1, im_height = 1, iteration = 100;
 	double x_center = 0, y_center = 0, width = 3, height = 3;
 
 	double xsqr, ysqr, xn, yn, xnp1, ynp1, mod;
@@ -30,14 +30,14 @@ int main()
 			x = x_center - width/2 + i*width/im_width;
 			y = y_center - height/2 + j*height/im_height;
 			xsqr = ysqr = 0;
-			xn = 0;
-			yn = 0;
+			xn = x;
+			yn = y;
 			in = true;
 
 			xpn = 1;
 			ypn = 0;
 
-			for(int k = 0; k <= iteration; k++)
+			for(int k = 0; k < iteration; k++)
 			{
 				/*2zn*zpn + 1 => 2 * (xn + i * yn) * (xpn + i * ypn) + 1
 							=> 2 * (xn * xpn - yn * ypn) + 1 + i * 2 * (xn * ypn + yn * xpn)*/
@@ -55,14 +55,15 @@ int main()
 				xnp1 = xsqr - ysqr + x;
 				ynp1 = 2 * xn * yn + y;
 
-				xn = xnp1;
-				yn = ynp1;
+				xn = 0;
+				yn = 0;
 
 				mod = sqrt(xsqr + ysqr);
 
 				if(mod >= 2)
 					in = false;
 
+				printf("%f, %f\n", mod, sqrt(xpn * xpn + ypn * ypn));
 				d[i][j][k] = mod * log(mod) / sqrt(xpn * xpn + ypn * ypn);
 			}
 			if(in)
@@ -77,7 +78,7 @@ int main()
 
 	FILE* file2 = fopen("test2.txt", "w+");
 
-	for(int k = 0; k <= iteration; k++)
+	for(int k = 0; k < iteration; k++)
 	{
 		fprintf(file2, "%5d ", k);
 		for(int j = 0; j < im_height; j++)

ae1b1975eb49e67770f93b89f3b17dc678295719
pepnou <pepnou1@gmail.com>
2019-02-22 12:39:04 +0100

    correction bug a l enregistrement
    mais apparition d un nouveau

--- matOp.cpp
+++ matOp.cpp
@@ -6,23 +6,18 @@ using namespace std;
 
 int matSave(Mat* mat, char* rep)
 {	
-	static int num = 0;
-	vector<int> compression_params;
+    static int num = 0;
+    vector<int> compression_params;
     compression_params.push_back( IMWRITE_PNG_COMPRESSION);
     compression_params.push_back(9);
 	
-	stringstream nom_img("");
-	nom_img << "mkdir -p ../img/" << rep << "/";
-	
-	system(nom_img.str().c_str());
-	
-	nom_img.str("");
-	nom_img << "../img/" << rep << "/mandel" << num++ << ".png";
+    stringstream nom_img("");
+    nom_img << rep << "/mandel" << num++ << ".png";
 
 
     cout << nom_img.str() << endl;
 
-	try
+    try
     {
         imwrite(nom_img.str().c_str(), *(mat), compression_params);
     }

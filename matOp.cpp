504b54890fd203eb904f8210a507d9e7712268f7
pepnou <pepnou1@gmail.com>
2019-03-27 13:18:58 +0100

    correction des bugs

--- matOp.cpp
+++ matOp.cpp
@@ -1,5 +1,11 @@
 #include "matOp.hpp"
 #include <mpi.h>
+#include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
 
 using namespace cv;
 using namespace std;
@@ -38,10 +44,16 @@ void img_init(char* rep, int img_num, int height, int width)
     stringstream entete("");
     entete << "P6\n"<< width << " " <<height << "\n256\n"; 
 
-    int file_section = open(nom_img, O_CREAT, 0600);
-    ftruncate(file_section, (height*width*3*sizeof(char)+strlen(entete)));
-    FILE *fd = fopen(nom_img, "w");
-    fprintf(fd, "%s", entete);
+    int file_section = open(nom_img.str().c_str(), O_CREAT, 0600);
+    int ret = ftruncate(file_section, height*width*3*sizeof(char) + entete.str().size());
+    if(ret == -1)
+    {
+        perror("ftruncate");
+        MPI_Abort(MPI_COMM_WORLD, 42);
+    }   
+
+    FILE *fd = fopen(nom_img.str().c_str(), "w");
+    fprintf(fd, "%s", entete.str().c_str());
     fclose(fd);
 }
 
@@ -50,18 +62,18 @@ void img_partial_save(int start, int width, int height, Mat* mat, char* rep, int
     stringstream nom_img("");
     nom_img << rep << "/mandel" << img_num << ".png";
     
-    char recopie = malloc(3*width*height*sizeof(char));
+    char* recopie = (char*)malloc(3*width*height*sizeof(char));
 
     for (int i = 0; i < width*height; i+=3)
     {
-        for (int j = 0; j < 3; ++)
+        for (int j = 0; j < 3; j++)
         {
             recopie[i+j] = mat->at<char>(i, j);
         }
     }
 
-    FILE *fd = fopen(nom_img, "w");
+    FILE *fd = fopen(nom_img.str().c_str(), "w");
     fseek(fd, start, SEEK_SET);
-    fwrite(recopie);
+    fwrite(recopie, sizeof(char), 3*width*height, fd);
     fclose(fd);
 }

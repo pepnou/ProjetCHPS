73059097a5e980e7d16d12625a010466e79d349a
enderswype <enderswype@orange.fr>
2019-03-27 09:57:58 +0000

    oui !

--- matOp.cpp
+++ matOp.cpp
@@ -29,28 +29,3 @@ void matSave(Mat* mat, char* rep, int img_num)
         fprintf(stderr, "Exception converting image to PNG format: %s\n", ex.what());
     }
 }
-
-int frameSave(Mat mat, char* rep, int num, int ligne)
-{
-	vector<int> compression_params;
-    compression_params.push_back( 16);
-    compression_params.push_back(9);
-	
-	stringstream nom_img("");
-	nom_img << "mkdir -p ../video/" << rep << "/frames/" << num << "/";
-	
-	system(nom_img.str().c_str());
-	
-	nom_img.str("");
-	nom_img << "../video/" << rep << "/frames/" << num << "/" << ligne << ".png";
-
-	try
-    {
-        imwrite(nom_img.str().c_str(), mat, compression_params);
-    }
-    catch (const Exception& ex)
-    {
-        fprintf(stderr, "Exception converting image to PNG format: %s\n", ex.what());
-    }
-    return num-1;
-}

3ddced74dc9faeef86ee123dc3fafdb0fc45f779
enderswype <enderswype@orange.fr>
2019-03-27 11:06:53 +0000

    changement de methode d'enregistrement

--- matOp.cpp
+++ matOp.cpp
@@ -29,3 +29,39 @@ void matSave(Mat* mat, char* rep, int img_num)
         fprintf(stderr, "Exception converting image to PNG format: %s\n", ex.what());
     }
 }
+
+void img_init(char* rep, int img_num, int height, int width)
+{
+    stringstream nom_img("");
+    nom_img << rep << "/mandel" << img_num << ".png";
+
+    stringstream entete("");
+    entete << "P6\n"<< width << " " <<height << "\n256\n"; 
+
+    int file_section = open(nom_img, O_CREAT, 0600);
+    ftruncate(file_section, (height*width*3*sizeof(char)+strlen(entete)));
+    FILE *fd = fopen(nom_img, "w");
+    fprintf(fd, "%s", entete);
+    fclose(fd);
+}
+
+void img_partial_save(int start, int width, int height, Mat* mat, char* rep, int img_num)
+{
+    stringstream nom_img("");
+    nom_img << rep << "/mandel" << img_num << ".png";
+    
+    char recopie = malloc(3*width*height*sizeof(char));
+
+    for (int i = 0; i < width*height; i+=3)
+    {
+        for (int j = 0; j < 3; ++)
+        {
+            recopie[i+j] = mat->at<char>(i, j);
+        }
+    }
+
+    FILE *fd = fopen(nom_img, "w");
+    fseek(fd, start, SEEK_SET);
+    fwrite(recopie);
+    fclose(fd);
+}
